cd55a64b3c8b5ebbabb953936bec80559318cc27
Van Kiet <vankietsvk@gmail.com>
2023-01-22 10:40:26 +0700

    step 16

---
+++ src/components/TestComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TestComponent extends React.Component{
+    render(){
+        return (         
+            <div>
+                Đây là Test component Của tui
+                
+            </div>
+        );
+    }
+}
+export default TestComponent;
