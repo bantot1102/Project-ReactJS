a8188fc6e75e1fdf1be3814146a54f96554188aa
Van Kiet <vankietsvk@gmail.com>
2023-01-22 12:01:06 +0700

    step 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,14 +7,25 @@ class MyComponent extends React.Component{
         address: `HCM`,
         school: `Nguyen Tat Thanh`
     };
+    // handleClick(){
+    //     console.log(`Click my pussy!`)
+    // };
+    handleClick = (event) =>{
+        console.log(`Check state`, this.state)
+    }
+    handleOnMouseOver = (event) =>{
+        console.log(`Check Event`, event.pageX, this.state.name )
+    }
 
     //JSX
     render() {
         return(
             <div>
                 My name is: {this.state.name}, 
-                I age: {this.state.age}, 
+                I age: {this.state.age}, ``
                 I live in: {this.state.address}
+                <button onClick={this.handleClick}>Click me!</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover me!</button>
             </div>
         );
     }
