6471d0c69af7df26880d117adc96cad67f127904
Van Kiet <vankietsvk@gmail.com>
2023-01-23 11:11:36 +0700

    step 25

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,23 +1,41 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+    state = {
+        isShowListUsers: true
+    }
+    handlShowHide = () =>{
+        this.setState({
+            isShowListUsers: !this.state.isShowListUsers
+        })
+    }
     render() {
-        const {listUsers} = this.props;
+        const { listUsers } = this.props;
 
         return (
+        <div>
             <div>
-            
-            {listUsers.map((user) =>{
+                <span onClick={()=>{ this.handlShowHide()}}>
+                {this.state.isShowListUsers === true ? `Hide list users` : `Show list users`}
+                </span>
+            </div>
+            {this.state.isShowListUsers &&
+            <div>
+            {listUsers.map((user) => {
                 return (
-                        <div key={user.id}>
-                            <div>Name is: {user.name}</div>
-                            <div>Age is: {user.age}</div>
-                            <hr/>
-                        </div>
-                )
+                <div
+                    key={user.id}
+                    className={+user.age < 23 ? "colorBlue" : "colorRed"}>
+                    {console.log(user.name)}
+                    <div>Name is: {user.name}</div>
+                    <div>Age is: {user.age}</div>
+                    <hr />
+                </div>
+                );
             })}
-            </div>  
-        )
+            </div>}
+        </div>
+        );
     }
 }
-export default DisplayInfo
+export default DisplayInfo;
