a484a0d1270742f542a9da05bd25526ed2231d2a
Van Kiet <vankietsvk@gmail.com>
2023-01-26 22:36:14 +0700

    chapter7-57

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -71,6 +71,7 @@ const ModalCreateUser = (props) => {
               <label className="form-label">Role</label>
               <select
                 className="form-select"
+                value={role}
                 onChange={(event) => setRole(event.target.value)}
               >
                 <option value="USER">USER</option>
